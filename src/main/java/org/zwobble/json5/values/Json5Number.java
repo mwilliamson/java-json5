668e8acd45dea42d734b8f71fa411780b66d0b98
Michael Williamson <mike@zwobble.org>
2025-01-01 22:31:49 +0000

    Remove unused import

--- src/main/java/org/zwobble/json5/values/Json5Number.java
+++ src/main/java/org/zwobble/json5/values/Json5Number.java
@@ -1,6 +1,4 @@
 package org.zwobble.json5.values;
 
-import org.zwobble.json5.sources.Json5SourceRange;
-
 public sealed interface Json5Number extends Json5Value permits Json5NumberFinite, Json5NumberNan, Json5NumberNegativeInfinity, Json5NumberPositiveInfinity {
 }
