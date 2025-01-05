2ed74a1785cfd67a9191111cd2bc66736b11b5d3
Michael Williamson <mike@zwobble.org>
2025-01-05 19:10:54 +0000

    Add Json5SourceRange.describe()

--- src/main/java/org/zwobble/json5/sources/Json5SourceRange.java
+++ src/main/java/org/zwobble/json5/sources/Json5SourceRange.java
@@ -43,4 +43,63 @@ public class Json5SourceRange {
             end.end
         );
     }
+
+    public String describe(String filename) {
+        var lineStartCharacterIndex = 0;
+        var lineIndex = 0;
+        var columnIndex = 0;
+
+        for (
+            var characterIndex = 0;
+            characterIndex < this.sourceText.characterLength();
+            characterIndex++
+        ) {
+            if (characterIndex == this.start.characterIndex()) {
+                var lineEndCharacterIndex = lineStartCharacterIndex;
+                while (
+                    lineEndCharacterIndex < this.sourceText.characterLength() &&
+                        this.sourceText.getCharacter(lineEndCharacterIndex) != '\n'
+                ) {
+                    lineEndCharacterIndex++;
+                }
+                return context(
+                    filename,
+                    this.sourceText.charBuffer(
+                        lineStartCharacterIndex,
+                        lineEndCharacterIndex
+                    ),
+                    lineIndex,
+                    columnIndex,
+                    lineEndCharacterIndex <= this.end.characterIndex()
+                        ? 1
+                        : Math.max(this.end.characterIndex() - this.start.characterIndex(), 1)
+                );
+            }
+
+            if (this.sourceText.getCharacter(characterIndex) == '\n') {
+                lineIndex += 1;
+                columnIndex = 0;
+                lineStartCharacterIndex = characterIndex + 1;
+            } else {
+                columnIndex += 1;
+            }
+        }
+
+        return context(
+            filename,
+            this.sourceText.charBuffer(
+                lineStartCharacterIndex,
+                this.sourceText.characterLength()
+            ),
+            lineIndex,
+            columnIndex,
+            1
+        );
+    }
+
+    private String context(String filename, CharBuffer line, int lineIndex, int columnIndex, int length) {
+        var lineNumber = lineIndex + 1;
+        var columnNumber = columnIndex + 1;
+        return filename + ":" + lineNumber + ":" + columnNumber + "\n" + line + "\n" + " ".repeat(columnIndex) + "^".repeat(length);
+    }
 }
