67024d744a9b344e10c91786b1451b394bf77367
Michael Williamson <mike@zwobble.org>
2026-07-30 22:38:02 +0100

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.zwobble.json5 {
+    requires transitive org.zwobble.sourcetext;
+
+    exports org.zwobble.json5.parser;
+    exports org.zwobble.json5.paths;
+    exports org.zwobble.json5.reader;
+    exports org.zwobble.json5.values;
+}
